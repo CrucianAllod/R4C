cf6bfadfede130fc6f7181a67ad4f1a1d554e810
CrucianAllod <k.viaznikov@yandex.ru>
2024-12-12 00:56:19 +0300

    Complete Task 2. Add endpoint for summary, add summary_robots func. Configured urls

--- robots/urls.py
+++ robots/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from robots.views import register_robot, success
+from robots.views import register_robot, success, summary_robots
 
 urlpatterns = [
     path('register/', register_robot, name='register_robot'),
+    path('summary_robots/', summary_robots, name='summary_robots'),
     path('succes/', success, name='success')
 ]
--- robots/views.py
+++ robots/views.py
@@ -1,8 +1,18 @@
+from datetime import timedelta
+from io import BytesIO
+from platform import version
+from turtledemo.penrose import start
+
+from django.db.models import Count
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
+from django.utils import timezone
+from numpy.ma.core import count
 
 from robots.forms import RobotForm
 from robots.models import Robot
+
+import pandas as pd
 
 
 def register_robot(request: HttpRequest) -> HttpResponse:
@@ -24,6 +34,53 @@ def register_robot(request: HttpRequest) -> HttpResponse:
         form = RobotForm()
 
     return render(request, 'robots/register_robot.html', {'form':form })
+
+def summary_robots(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+
+        end_date = timezone.now()
+        start_date = end_date - timedelta(days=30)
+
+        robots = (
+            Robot.objects.filter(created__range=(start_date, end_date))
+            .values('model', 'version')
+            .annotate(count=Count('id'))
+        )
+
+        model_dataframes = {}
+
+        for robot in robots:
+            print(robot)
+            model = robot['model']
+            version = robot['version']
+            count = robot['count']
+
+            if model not in model_dataframes:
+                model_dataframes[model] = {
+                    'Модель': [],
+                    'Версия': [],
+                    'Количество за неделю': []
+                }
+
+            model_dataframes[model]['Модель'].append(model)
+            model_dataframes[model]['Версия'].append(version)
+            model_dataframes[model]['Количество за неделю'].append(count)
+
+        output = BytesIO()
+        with pd.ExcelWriter(output, engine='openpyxl') as writer:
+            for model, data in model_dataframes.items():
+                df = pd.DataFrame(data)
+                df.to_excel(writer, sheet_name=model,  index=False, engine='openpyxl')
+
+        output.seek(0)
+
+        response = HttpResponse(
+            output,
+            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
+        response['Content-Disposition'] = 'attachment; filename="robots_summary.xlsx"'
+
+        return response
 
 def success(request: HttpRequest) -> HttpResponse:
     return render(request, 'robots/success.html')
